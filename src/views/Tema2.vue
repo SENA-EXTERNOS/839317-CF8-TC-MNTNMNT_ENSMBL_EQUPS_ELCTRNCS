bfb2975413a9123ad6d4ea13fab757b3b8f72578
elrilacho <camilobr17@gmail.com>
2023-03-30 16:19:56 -0500

    Componente listo

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -78,7 +78,7 @@
         iframe(
           width='560',
           height='315',
-          src='https://www.youtube.com/embed/2L91WMqw96A',
+          src='https://www.youtube.com/embed/G3v1xfR_Md4',
           title='YouTube video player',
           frameborder='0',
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
